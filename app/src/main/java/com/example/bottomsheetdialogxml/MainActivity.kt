d43585e595c93c7c3449c073d9b527ee1c08fd0a
FATIQ HUSSNAIN <fatiqhussnain1@gmail.com>
2023-08-02 08:11:00 -0700

    Bottom Sheet Dialog done

--- app/src/main/java/com/example/bottomsheetdialogxml/MainActivity.kt
+++ app/src/main/java/com/example/bottomsheetdialogxml/MainActivity.kt
@@ -4,8 +4,9 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
 import android.widget.TextView
+import com.example.bottomsheetdialogxml.dialogs.BottomSheetDialog
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), BottomSheetDialog.BottomSheetListener {
 
     private lateinit var openDialogSheet: Button
     private lateinit var textViewButton: TextView
@@ -13,5 +14,19 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
+        openDialogSheet = findViewById(R.id.btn_open_dialog)
+        textViewButton = findViewById(R.id.textView)
+
+        openDialogSheet.setOnClickListener {
+            val myBottomSheetDialog = BottomSheetDialog()
+            myBottomSheetDialog.show(supportFragmentManager, "Dialog")
+        }
+
+    }
+
+    override fun onButtonClicked(input: String) {
+        textViewButton.text = input
     }
 }
--- app/src/main/java/com/example/bottomsheetdialogxml/dialogs/BottomSheetDialog.kt
+++ app/src/main/java/com/example/bottomsheetdialogxml/dialogs/BottomSheetDialog.kt
@@ -1,17 +1,18 @@
 package com.example.bottomsheetdialogxml.dialogs
 
+import android.content.Context
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
-import androidx.fragment.app.DialogFragment
 import com.example.bottomsheetdialogxml.R
 import com.google.android.material.bottomsheet.BottomSheetDialogFragment
+import kotlin.RuntimeException
 
 class BottomSheetDialog : BottomSheetDialogFragment() {
 
+    private lateinit var listener: BottomSheetListener
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?,
@@ -23,16 +24,31 @@ class BottomSheetDialog : BottomSheetDialogFragment() {
         val button2: Button = view.findViewById(R.id.button2)
 
         button1.setOnClickListener {
-
+            listener.onButtonClicked("Button 1 clicked")
+            dismiss()
         }
 
         button2.setOnClickListener {
-
-
+            listener.onButtonClicked("Button 2 clicked")
+            dismiss()
         }
 
         return view
     }
 
 
+    interface BottomSheetListener {
+        fun onButtonClicked(input: String)
+    }
+
+
+    override fun onAttach(context: Context) {
+        super.onAttach(context)
+        if (context is BottomSheetListener) {
+            listener = context
+        } else {
+            throw RuntimeException(context.toString() +
+            "You must implement BottomSheetListener ")
+        }
+    }
 }
